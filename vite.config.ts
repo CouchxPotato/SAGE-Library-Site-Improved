d4aa538418b7b9f70b16584ff161d6dcb249f020
CouchxPotato <jainaneesh18@gmail.com>
2025-01-28 17:15:35 +0530

    added hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true
+    host: true,
+    allowedHosts:['sage-central-library.onrender.com'], //add the allowed hosts here
   }
 });
