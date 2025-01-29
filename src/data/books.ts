888480cd551c1904d4ca2f461504f11ecbf7a6e6
Aki <122743372+CouchxPotato@users.noreply.github.com>
2025-01-29 14:00:38 +0530

    Update books.ts

--- src/data/books.ts
+++ src/data/books.ts
@@ -1,171 +1,190 @@
-export interface Book {
-  title: string;
-  shelf: number;
-}
-
-export interface Almirah {
-  id: string;
-  books: Book[];
-}
-
 export const almirahs: Almirah[] = [
   {
-    id: "A-001",
-    books: [
-      { title: "Digital image Processing and Computer Vision By Sonka, Milan", shelf: 1 },
-      { title: "Concept of Programming Languages  By Robert W. Sebesta", shelf: 2 },
-      { title: "Theory of Machines By R. S. Khurmi", shelf: 3 },
-      { title: "Theory of Machines By R. S. Khurmi", shelf: 4 },
-      { title: "Theory of Machines By R. S. Khurmi", shelf: 5 },
-      { title: "T for Librarians By Ansari, K. Ravindran", shelf: 6 }
-    ]
+    id: "001",
+    subAlmirahs: {
+      a: {
+        id: "A-001",
+        books: [
+          { title: "Digital image Processing and Computer Vision By Sonka, Milan", shelf: 1 },
+          { title: "Concept of Programming Languages  By Robert W. Sebesta", shelf: 2 },
+          { title: "Theory of Machines By R. S. Khurmi", shelf: 3 },
+          { title: "Theory of Machines By R. S. Khurmi", shelf: 4 },
+          { title: "Theory of Machines By R. S. Khurmi", shelf: 5 },
+          { title: "T for Librarians By Ansari, K. Ravindran", shelf: 6 }
+        ]
+      },
+      b: {
+        id: "B-001",
+        books: [
+          { title: "Data Structures By Seymour Lipschut", shelf: 1 },
+          { title: "Data Structures By Seymour Lipschut", shelf: 2 },
+          { title: "Data Structures By Seymour Lipschut", shelf: 3 },
+          { title: "Advanced Computer Architecture by Kai Hwang", shelf: 4 },
+          { title: "Concepts in Engineering Design By R. K. Gupta", shelf: 5 },
+          { title: "Concepts in Engineering Design By R. K. Gupta", shelf: 6 }
+        ]
+      }
+    }
   },
   {
-    id: "B-001",
-    books: [
-      { title: "Data Structures By Seymour Lipschut", shelf: 1 },
-      { title: "Data Structures By Seymour Lipschut", shelf: 2 },
-      { title: "Data Structures By Seymour Lipschut", shelf: 3 },
-      { title: "Advanced Computer Architecture by Kai Hwang", shelf: 4 },
-      { title: "Concepts in Engineering Design By R. K. Gupta", shelf: 5 },
-      { title: "Concepts in Engineering Design By R. K. Gupta", shelf: 6 }
-    ]
+    id: "002",
+    subAlmirahs: {
+      a: {
+        id: "A-002",
+        books: [
+          { title: "Thermal Engineering By R. K. Rajput", shelf: 1 },
+          { title: "Linear Integrated Circuits by Roy Choudhary", shelf: 2 },
+          { title: "T. B. of Thermal Engineering K. S. Khurmi", shelf: 3 },
+          { title: "Electromagnetic Waves And Radiating Systems By Edward C. Jordan", shelf: 4 },
+          { title: "Object Oriented Modeling and Design with UML by Michael Blaha", shelf: 5 },
+          { title: "Monochrome and Colour Television By R. R. Gulati", shelf: 6 }
+        ]
+      },
+      b: {
+        id: "B-002",
+        books: [
+          { title: "Textbook of Fluid Mechanics and Hydraulic Machines By  RK Rajput", shelf: 1 },
+          { title: "Circuit Theory By A. Chakrabarti", shelf: 2 },
+          { title: "Circuit Theory By A. Chakrabarti", shelf: 3 },
+          { title: "Circuit Theory By A. Chakrabarti", shelf: 4 },
+          { title: "Circuit Theory By A. Chakrabarti", shelf: 5 },
+          { title: "Circuit Theory By A. Chakrabarti", shelf: 6 }
+        ]
+      }
+    }
   },
   {
-    id: "A-002",
-    books: [
-      { title: "Thermal Engineering By R. K. Rajput", shelf: 1 },
-      { title: "Linear Integrated Circuits by Roy Choudhary", shelf: 2 },
-      { title: "T. B. of Thermal Engineering K. S. Khurmi", shelf: 3 },
-      { title: "Electromagnetic Waves And Radiating Systems By Edward C. Jordan", shelf: 4 },
-      { title: "Object Oriented Modeling and Design with UML by Michael Blaha", shelf: 5 },
-      { title: "Monochrome and Colour Television By R. R. Gulati", shelf: 6 }
-    ]
+    id: "003",
+    subAlmirahs: {
+      a: {
+        id: "A-003",
+        books: [
+          { title: "Communication Systems Engineering By  John G Proakis", shelf: 1 },
+          { title: "Communication Systems Engineering By  Simon Haykin", shelf: 1 },
+          { title: "Engineering Thermodynamics By P. K. Nag", shelf: 1 },
+          { title: "Problem Solving and Program Design in C By Elliot Koffman", shelf: 2 },
+          { title: "Microprocessor Information By  N Senthil Kumar", shelf: 2 },
+          { title: "Object Oriented Analysis and Design with Applications By Grady Booch", shelf: 3 },
+          { title: "Monochrome and Colour Television By R. K. Gulati", shelf: 4 },
+          { title: "Monochrome and Colour Television By R. K. Gulati", shelf: 5 },
+          { title: "Engineering Electromagnatics By  W. H. Hayt", shelf: 5 },
+          { title: "Engineering Electromagnatics By  W. H. Hayt", shelf: 6 },
+          { title: "Monochrome and Colour Television By R. K. Gulati", shelf: 6 }
+        ]
+      },
+      b: {
+        id: "B-003",
+        books: [
+          { title: "Programming in Visual Basic 6.0 By Bradley Julia", shelf: 1 },
+          { title: "Programming in Visual Basic 6.0 By Bradley Julia", shelf: 2 },
+          { title: "Programming in Visual Basic 6.0 By Bradley Julia", shelf: 3 },
+          { title: "Introduction to Unix and Shell Programming By M. G. Ventca", shelf: 3 },
+          { title: "Object-Oriented Programming with C++ By E. Balagurusamy", shelf: 4 },
+          { title: "Object-Oriented Programming with C++ By E. Balagurusamy", shelf: 5 },
+          { title: "Principles of Programming Language By Rajiv Chopra", shelf: 6 },
+          { title: "Programming Language By Pratt W. Terrace", shelf: 6 }
+        ]
+      }
+    }
   },
   {
-    id: "B-002",
-    books: [
-      { title: "Textbook of Fluid Mechanics and Hydraulic Machines By  RK Rajput", shelf: 1 },
-      { title: "Circuit Theory By A. Chakrabarti", shelf: 2 },
-      { title: "Circuit Theory By A. Chakrabarti", shelf: 3 },
-      { title: "Circuit Theory By A. Chakrabarti", shelf: 4 },
-      { title: "Circuit Theory By A. Chakrabarti", shelf: 5 },
-      { title: "Circuit Theory By A. Chakrabarti", shelf: 6 }
-    ]
+    id: "004",
+    subAlmirahs: {
+      a: {
+        id: "A-004",
+        books: [
+          { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 1 },
+          { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 2 },
+          { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 3 },
+          { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 4 },
+          { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 5 },
+          { title: "Manet & High Speed Network By Pankaj Sharma", shelf: 6 },
+          { title: "Fundamentals of Electronic Engineering By Saket Verma", shelf: 6 },
+          { title: "Electronics Devices & Circuit Theory By Robert L. Boylestad", shelf: 6 },
+          { title: "Electronic Circuit By Donald A Neamen", shelf: 6 },
+          { title: "Classical Electroddynamics By Johan David Jordan", shelf: 6 },
+          { title: "Electronic Circuits By A. P. Godse", shelf: 6 },
+          { title: "Basic Electronics J. B. Gupta / S. K. Vijay / Shiv Shankar Mishra", shelf: 6 },
+          { title: "Funcamental of Electronic Engineering By J. S. Katre", shelf: 6 },
+          { title: "2000 Solved Problems in Digital Electronic By S. P. Bali", shelf: 6 },
+          { title: "Digital Electronics By J. S. Katre", shelf: 6 },
+          { title: "Information Storage & Management By Pankaj Sharma", shelf: 6 }
+        ]
+      },
+      b: {
+        id: "B-004",
+        books: [
+          { title: "A Textbook of Fluid Mechanics and Hydraulic Machines By R. K. Bansal", shelf: 1 },
+          { title: "Microprocessors and Microcontrollers By Kani, A. Nagoor", shelf: 2 },
+          { title: "Digital Design By John K. Wakerly", shelf: 3 },
+          { title: "Fundamental of Electronic Engineering By Akhilesh K. Upadhyay", shelf: 3 },
+          { title: "Introduction to Nanoscience and Nanotechnology By Chattopadhyay K.K", shelf: 3 },
+          { title: "CMOS VLS Design By Neil H. E. Weste", shelf: 4 },
+          { title: "Problems in Operations Research By Prem Kumar Gupta", shelf: 5 },
+          { title: "Problems in Operations Research By Prem Kumar Gupta", shelf: 6 }
+        ]
+      }
+    }
   },
   {
-    id: "A-003",
-    books: [
-      { title: "Communication Systems Engineering By  John G Proakis", shelf: 1 },
-      { title: "Communication Systems Engineering By  Simon Haykin", shelf: 1 },
-      { title: "Engineering Thermodynamics By P. K. Nag", shelf: 1 },
-      { title: "Problem Solving and Program Design in C By Elliot Koffman", shelf: 2 },
-      { title: "Microprocessor Information By  N Senthil Kumar", shelf: 2 },
-      { title: "Object Oriented Analysis and Design with Applications By Grady Booch", shelf: 3 },
-      { title: "Monochrome and Colour Television By R. K. Gulati", shelf: 4 },
-      { title: "Monochrome and Colour Television By R. K. Gulati", shelf: 5 },
-      { title: "Engineering Electromagnatics By  W. H. Hayt", shelf: 5 },
-      { title: "Engineering Electromagnatics By  W. H. Hayt", shelf: 6 },
-      { title: "Monochrome and Colour Television By R. K. Gulati", shelf: 6 }
-    ]
+    id: "005",
+    subAlmirahs: {
+      a: {
+        id: "A-005",
+        books: [
+          { title: "Network Analysis By G. K. Mittal", shelf: 1 },
+          { title: "Network Analysis By G. K. Mittal", shelf: 2 },
+          { title: "Network Analysis By Van Valkenbury", shelf: 3 },
+          { title: "Network Analysis By Van Valkenbury", shelf: 4 },
+          { title: "Network Analysis By G. K. Mithal", shelf: 5 },
+          { title: "Network Analysis By G. K. Mithal", shelf: 6 }
+        ]
+      },
+      b: {
+        id: "B-005",
+        books: [
+          { title: "Optical Fiber Communication By Gerd Keiser", shelf: 1 },
+          { title: "Optical Fiber Communication By Gerd Keiser", shelf: 2 },
+          { title: "Utilization of Electric Power & Electric Traction By  J.B. Gupta", shelf: 3 },
+          { title: "Entrepreneurship By David H. Hat", shelf: 4 },
+          { title: "IT Strategy and Management By Sanjiva Shankar Dubey", shelf: 5 },
+          { title: "Design OF Transformaers By Dasgupta, Indrajit", shelf: 6 }
+        ]
+      }
+    }
   },
   {
-    id: "B-003",
-    books: [
-      { title: "Programming in Visual Basic 6.0 By Bradley Julia", shelf: 1 },
-      { title: "Programming in Visual Basic 6.0 By Bradley Julia", shelf: 2 },
-      { title: "Programming in Visual Basic 6.0 By Bradley Julia", shelf: 3 },
-      { title: "Introduction to Unix and Shell Programming By M. G. Ventca", shelf: 3 },
-      { title: "Object-Oriented Programming with C++ By E. Balagurusamy", shelf: 4 },
-      { title: "Object-Oriented Programming with C++ By E. Balagurusamy", shelf: 5 },
-      { title: "Principles of Programming Language By Rajiv Chopra", shelf: 6 },
-      { title: "Programming Language By Pratt W. Terrace", shelf: 6 }
-    ]
-  },
-  {
-    id: "A-004",
-    books: [
-      { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 1 },
-      { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 2 },
-      { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 3 },
-      { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 4 },
-      { title: "Data communication and Networking By Behrouz  Forouzan", shelf: 5 },
-      { title: "Manet & High Speed Network By Pankaj Sharma", shelf: 6 },
-      { title: "Fundamentals of Electronic Engineering By Saket Verma", shelf: 6 },
-      { title: "Electronics Devices & Circuit Theory By Robert L. Boylestad", shelf: 6 },
-      { title: "Electronic Circuit By Donald A Neamen", shelf: 6 },
-      { title: "Classical Electroddynamics By Johan David Jordan", shelf: 6 },
-      { title: "Electronic Circuits By A. P. Godse", shelf: 6 },
-      { title: "Basic Electronics J. B. Gupta / S. K. Vijay / Shiv Shankar Mishra", shelf: 6 },
-      { title: "Funcamental of Electronic Engineering By J. S. Katre", shelf: 6 },
-      { title: "2000 Solved Problems in Digital Electronic By S. P. Bali", shelf: 6 },
-      { title: "Digital Electronics By J. S. Katre", shelf: 6 },
-      { title: "Information Storage & Management By Pankaj Sharma", shelf: 6 }
-    ]
-  },
-  {
-    id: "B-004",
-    books: [
-      { title: "A Textbook of Fluid Mechanics and Hydraulic Machines By R. K. Bansal", shelf: 1 },
-      { title: "Microprocessors and Microcontrollers By Kani, A. Nagoor", shelf: 2 },
-      { title: "Digital Design By John K. Wakerly", shelf: 3 },
-      { title: "Fundamental of Electronic Engineering By Akhilesh K. Upadhyay", shelf: 3 },
-      { title: "Introduction to Nanoscience and Nanotechnology By Chattopadhyay K.K", shelf: 3 },
-      { title: "CMOS VLS Design By Neil H. E. Weste", shelf: 4 },
-      { title: "Problems in Operations Research By Prem Kumar Gupta", shelf: 5 },
-      { title: "Problems in Operations Research By Prem Kumar Gupta", shelf: 6 }
-    ]
-  },
-  {
-    id: "A-005",
-    books: [
-      { title: "Network Analysis By G. K. Mittal", shelf: 1 },
-      { title: "Network Analysis By G. K. Mittal", shelf: 2 },
-      { title: "Network Analysis By Van Valkenbury", shelf: 3 },
-      { title: "Network Analysis By Van Valkenbury", shelf: 4 },
-      { title: "Network Analysis By G. K. Mithal", shelf: 5 },
-      { title: "Network Analysis By G. K. Mithal", shelf: 6 }
-    ]
-  },
-  {
-    id: "B-005",
-    books: [
-      { title: "Optical Fiber Communication By Gerd Keiser", shelf: 1 },
-      { title: "Optical Fiber Communication By Gerd Keiser", shelf: 2 },
-      { title: "Utilization of Electric Power & Electric Traction By  J.B. Gupta", shelf: 3 },
-      { title: "Entrepreneurship By David H. Hat", shelf: 4 },
-      { title: "IT Strategy and Management By Sanjiva Shankar Dubey", shelf: 5 },
-      { title: "Design OF Transformaers By Dasgupta, Indrajit", shelf: 6 }
-    ]
-  },
-  {
-    id: "A-006",
-    books: [
-      { title: "Success Mantra of Brhamhos By A. Sivathanu Pillai", shelf: 1 },
-      { title: "Success Mantra of Brhamhos By A. Sivathanu Pillai", shelf: 2 },
-      { title: "Network Analysis By G. K. Mithal", shelf: 3 },
-      { title: "An Introduction to Data Structures with Application ByTremblay Jean-Paul", shelf: 4 },
-      { title: "Integrated Electronics By Millman-Halkias", shelf: 4 },
-      { title: "Integrated Electronics By Millman-Halkias", shelf: 5 },
-      { title: "Integrated Electronics By Millman-Halkias", shelf: 6 }
-    ]
-  },
-  {
-    id: "B-006",
-    books: [
-      { title: "Mobile Cellular Telecommunications By William C y Lee", shelf: 1 },
-      { title: "Mobile Cellular Telecommunications By William C y Lee", shelf: 2 },
-      { title: "Mobile Cellular Telecommunications By William C y Lee", shelf: 3 },
-      { title: "Fiber-optic communication systems By Govind P. Agrawal", shelf: 4 },
-      { title: "Wireless and Mobile Communication ByT. G. Palanivelu", shelf: 4 },
-      { title: "Fundamentals of Python By Kenneth Alfred Lambert", shelf: 4 },
-      { title: "Intro to Wireless and Mobile Systems By Dharma Prakash Agrawal", shelf: 4 },
-      { title: "Wireless Communications & Networks By William Stallings", shelf: 4 },
-      { title: "Wireless Digital Communications By Kamilo Feher", shelf: 4 },
-      { title: "Wireless Communications By ChidambaraNathan, P. Muthu", shelf: 4 },
-      { title: "HTML 5 Black Book By Editorial Services", shelf: 4 },
-      { title: "Neural Networks Fuzzy Logic and Genetic Algorithms By G. A. VIJAYALAKSHMI PAI", shelf: 5 },
-      { title: "Object Oriented Programming with C++ By David Parsons", shelf: 6 }
-    ]
+    id: "006",
+    subAlmirahs: {
+      a: {
+        id: "A-006",
+        books: [
+          { title: "Success Mantra of Brhamhos By A. Sivathanu Pillai", shelf: 1 },
+          { title: "Success Mantra of Brhamhos By A. Sivathanu Pillai", shelf: 2 },
+          { title: "Network Analysis By G. K. Mithal", shelf: 3 },
+          { title: "An Introduction to Data Structures with Application ByTremblay Jean-Paul", shelf: 4 },
+          { title: "Integrated Electronics By Millman-Halkias", shelf: 5 },
+          { title: "Integrated Electronics By Millman-Halkias", shelf: 6 }
+        ]
+      },
+      b: {
+        id: "B-006",
+        books: [
+          { title: "Mobile Cellular Telecommunications By William C y Lee", shelf: 1 },
+          { title: "Mobile Cellular Telecommunications By William C y Lee", shelf: 2 },
+          { title: "Mobile Cellular Telecommunications By William C y Lee", shelf: 3 },
+          { title: "Fiber-optic communication systems By Govind P. Agrawal", shelf: 4 },
+          { title: "Wireless and Mobile Communication ByT. G. Palanivelu", shelf: 4 },
+          { title: "Fundamentals of Python By Kenneth Alfred Lambert", shelf: 4 },
+          { title: "Intro to Wireless and Mobile Systems By Dharma Prakash Agrawal", shelf: 4 },
+          { title: "Wireless Communications & Networks By William Stallings", shelf: 4 },
+          { title: "Wireless Digital Communications By Kamilo Feher", shelf: 4 },
+          { title: "Wireless Communications By ChidambaraNathan, P. Muthu", shelf: 4 },
+          { title: "HTML 5 Black Book By Editorial Services", shelf: 4 },
+          { title: "Neural Networks Fuzzy Logic and Genetic Algorithms By G. A. VIJAYALAKSHMI PAI", shelf: 5 },
+          { title: "Object Oriented Programming with C++ By David Parsons", shelf: 6 }
+        ]
+      }
+    }
   }
 ];
